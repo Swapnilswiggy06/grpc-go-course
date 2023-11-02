482da03e2b54030fc44c17dd7378895274a092a2
Swapnilswiggy06 <swapnil.singh_int@external.swiggy.in>
2023-11-02 17:23:15 +0530

    feat: add sum api

---
+++ calculator/client/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/Clement-Jean/grpc-go-course/calculator/proto"
+)
+
+var addr string = "localhost:50051"
+
+func main() {
+
+	// did like this because of ssl error
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("Failed to dial: %v", err)
+	}
+
+	defer conn.Close()
+
+	c := pb.NewCalculatorServiceClient(conn)
+
+	doSum(c)
+}
